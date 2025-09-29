70fc9c1e936a4ff04048ee3c84112ec39f8f0ec8
1stprinciple <ztyharbin@gmail.com>
2025-09-29 02:49:44 +0200

    self.tokenizer = tokenizer

--- rllm/engine/rollout/fireworks_engine.py
+++ rllm/engine/rollout/fireworks_engine.py
@@ -14,6 +14,7 @@ class FireworksEngine(OpenAIEngine):
     def __init__(
         self,
         model: str,
+        tokenizer=None,
         api_retries: int = 3,
         base_url: str = "https://api.fireworks.ai/inference/v1",
         api_key: str = os.getenv("FIREWORKS_API_KEY"),
@@ -24,7 +25,7 @@ class FireworksEngine(OpenAIEngine):
         self.api_retries = api_retries
         self.sampling_params = sampling_params or {}
         self._use_chat_completions = True
-
+        self.tokenizer = tokenizer
         self.client = openai.AsyncOpenAI(base_url=base_url, api_key=api_key)
         logging.getLogger("httpx").setLevel(logging.WARNING)
 
--- rllm/trainer/verl/agent_workflow_trainer_fireworks.py
+++ rllm/trainer/verl/agent_workflow_trainer_fireworks.py
@@ -74,8 +74,10 @@ class PipelineAgentWorkflowPPOTrainer(AgentWorkflowPPOTrainer):
         #     worker_group=self.rollout_wg,
         # )
 
+        from transformers import AutoTokenizer
         rollout_engine = FireworksEngine(
             model="accounts/fireworks/models/qwen3-30b-a3b-instruct-2507",
+            tokenizer=AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
             sampling_params={"temperature": 0.6, "top_p": 0.95, "max_tokens": 2048},
         )
 
